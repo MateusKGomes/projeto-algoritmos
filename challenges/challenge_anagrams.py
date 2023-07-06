75eb4d492292995491b97f1dc77c5524a41fbc25
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-06 18:31:56 -0300

    Requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,42 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+def is_anagram(first_string: str, second_string: str):
+    if first_string == '' and second_string == '':
+        return ('', '', False)
+    
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    ordered_first = merge_sort(first)
+    ordered_second = merge_sort(second)
+
+    if ordered_first == ordered_second:
+        return (''.join(ordered_first), ''.join(ordered_second), True)
+    
+    return (''.join(ordered_first), ''.join(ordered_second), False)
