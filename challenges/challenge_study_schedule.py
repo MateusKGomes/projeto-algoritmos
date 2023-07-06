c9dbe4fc128cfb1558fd4a60eb62bd92dac2522c
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-06 15:18:44 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    best_period = 0
+    if not isinstance(target_time, int):
+        return None
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            best_period += 1
+    return best_period
