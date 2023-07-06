2b291deb83afbdea1929b25390342d026cbfe4ce
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-06 16:10:27 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(False, 2)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("arara", "not int")
+    assert encrypt_message("arara", 2) == "ara_ra"
+    assert encrypt_message("arara", 9) == "arara"
