978fd5b5b9ab9925629b2d6424cf4a82adf15e6d
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-07 15:46:39 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
+from challenges.utils.order import merge_sort
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    ordered = merge_sort(nums)
+    for num in range(0, len(ordered) -1):
+        if not isinstance(ordered[num], int) or ordered[num] < 0:
+            return False
+        if ordered[num] == ordered[num + 1]:
+            return ordered[num]
+    return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str):
+    reverse_word = list(word).reverse()
+
+    if word == ''.join(reverse_word):
+        return True
+
+    return reverse_word
+
+print(is_palindrome_iterative('olá'))
