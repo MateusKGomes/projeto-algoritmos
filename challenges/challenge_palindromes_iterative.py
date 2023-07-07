b78ac7b6089bc222d89f6859017aa0d2b0b18c1d
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-07 17:44:08 -0300

    Requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,10 +1,13 @@
 def is_palindrome_iterative(word: str):
-    reverse_word = list(word).reverse()
+    arr_word = list(word)
+    new_word = []
+    if word == "":
+        return False
 
-    if word == "".join(reverse_word):
+    for i in range(len(arr_word) - 1, -1, -1):
+        new_word.append(arr_word[i])
+
+    if arr_word == new_word:
         return True
 
-    return reverse_word
-
-
-print(is_palindrome_iterative("olá"))
+    return False
