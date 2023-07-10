6b3bbe3d9560ead4590c690fa6c7dd82717a42ae
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-10 17:50:31 -0300

    refatora requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,8 +1,8 @@
 from challenges.utils.order import merge_sort
 
 
-def find_duplicate(nums):
-    ordered = merge_sort(nums)
+def find_duplicate(nums: list):
+    ordered = sorted(nums)
     for num in range(0, len(ordered) - 1):
         if not isinstance(ordered[num], int) or ordered[num] < 0:
             return False
